5862289ea648d237ddbdb77cc6f58dc2ec7a5cd2
NikolaStefanovski <nikolastefanovski992@gmail.com>
2019-07-10 17:01:29 +0200

    init commit

---
+++ OdeToFoodFromScratch/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Restaurant 1", Location = "Kumanovo", Cuisine = CuisineType.Indian },
+
+                new Restaurant { Id = 2, Name = "Restaurant 2", Location = "Skopje", Cuisine = CuisineType.Mexican }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants;
+        }
+    }
+}
